da09934b2b34cfb849427363339ec9d7009884a3
Kronos493 <patchara.ing@gmail.com>
2013-11-20 14:20:18 +0700

    Add relation Add assign

--- lib/address.rb
+++ lib/address.rb
@@ -1,5 +1,11 @@
 class Address < ActiveRecord::Base
+  attr_accessible :postal_code_id, :building_number, :floor, :latitude, :longtitude
+
   translates :building_name, :street_name, 
     :street_name, :province_name, :district_name, :sub_district_name,
     :extra_info
+
+  belongs_to :addressable, polymorphic: true
+
+  belongs_to :postal_code
 end
