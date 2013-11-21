40aa631c55812e03edcdc7f21200293eb1056b27
Kronos493 <patchara.ing@gmail.com>
2013-11-21 16:53:09 +0700

    Test dynamic validate

--- lib/address.rb
+++ lib/address.rb
@@ -7,16 +7,24 @@ class Address < ActiveRecord::Base
 
   belongs_to :postal_code
 
-
+  validate :dynamic_validation
 
   def fetch_data_from_postal_code
     if postal_code
-      Setting.available_locales.each do |x|
-        Globalize.with_locale(x) do
+      Setting.available_locales.each do |lang|
+        Globalize.with_locale(lang) do
           self.assign_attributes(postal_code.locality)
         end
       end
       nil
+    end
+  end
+
+  def dynamic_validation
+    if Setting.try("#{addressable_type}_address").present?
+      Setting.try("#{addressable_type}_address").each do |key, value|
+        errors.add(key, t('errors.messages.blank')) if value && try(key).blank?
+      end
     end
   end
 end
