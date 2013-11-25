379b8e85072f0b4fe1727e9e4dc95a313e5e8f3f
Kronos493 <patchara.ing@gmail.com>
2013-11-25 16:11:05 +0700

    Add road translate

--- lib/address.rb
+++ lib/address.rb
@@ -1,6 +1,6 @@
 class Address < ActiveRecord::Base
   translates :building_name, :street_name, 
-    :street_name, :province_name, :district_name, :sub_district_name,
+    :road, :province_name, :district_name, :sub_district_name,
     :extra_info
 
   validates :addressable, presence: true
