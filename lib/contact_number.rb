e015461c5ab506fbcea264d5186342c9c04fd740
Kronos493 <patchara.ing@gmail.com>
2013-12-10 13:36:18 +0700

    Add validate contact type

--- lib/contact_number.rb
+++ lib/contact_number.rb
@@ -1,3 +1,5 @@
 class ContactNumber < ActiveRecord::Base
   belongs_to :contactable, polymorphic: true
+  CONTACT_TYPE = %w[mobile landline]
+  validates :contact_type, inclusion: { :in => CONTACT_TYPE }
 end
