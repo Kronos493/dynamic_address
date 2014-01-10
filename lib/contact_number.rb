f8d754582b4f23980416fb0349b6c4fd0e2a98b5
Jo <jo@smartsoftasia.com>
2014-01-10 18:10:26 +0700

    v.0.0.2  -- add contact types 'fax'

--- lib/contact_number.rb
+++ lib/contact_number.rb
@@ -1,6 +1,6 @@
 class ContactNumber < ActiveRecord::Base
   belongs_to :contactable, polymorphic: true
-  CONTACT_TYPE = %w[mobile landline]
+  CONTACT_TYPE = %w[mobile landline fax]
   validates :contact_type, inclusion: { :in => CONTACT_TYPE }
   validates :number, presence: true
 end
