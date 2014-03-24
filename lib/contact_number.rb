c495558abd7fd157580cb5473bd7cfa0b9c4e660
Kronos493 <patchara.ing@gmail.com>
2014-03-24 15:28:20 +0700

    Add main_number

--- lib/contact_number.rb
+++ lib/contact_number.rb
@@ -3,4 +3,9 @@ class ContactNumber < ActiveRecord::Base
   CONTACT_TYPE = %w[mobile landline fax]
   validates :contact_type, inclusion: { :in => CONTACT_TYPE }
   validates :number, presence: true
+
+
+  def self.main_number
+    where(is_main: true).first
+  end
 end
