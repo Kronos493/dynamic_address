d1720470b74f8797eb7867e5941058d0016168b9
Kronos493 <patchara.ing@gmail.com>
2014-03-26 12:46:27 +0700

    Add scope type

--- lib/contact_number.rb
+++ lib/contact_number.rb
@@ -5,6 +5,10 @@ class ContactNumber < ActiveRecord::Base
   validates :number, presence: true
 
 
+  CONTACT_TYPE.each do |contact_type|
+    scope contact_type, -> { where(contact_type: contact_type) }
+  end
+
   def self.main_number
     where(is_main: true).first
   end
