effc447c5853957dbc604189baa52727079a041a
Patorn Utenpattanun <patorn.u@gmail.com>
2014-04-09 16:46:21 +0700

    Add rds money

---
+++ lib/rds_money.rb
@@ -0,0 +1,26 @@
+module RdsMoney
+  extend ActiveSupport::Concern
+
+  def display_money(amount)
+    "#{currency}#{display_amount(amount)}"
+  end
+
+  def display_amount(amount)
+    amount ||= 0
+    sprintf "%.2f", amount
+  end
+
+  def currency
+    self[:currency] || Setting[:default_currency]
+  end
+
+  module ClassMethods
+    def define_display_money!(*args)
+      args.each do |arg|
+        define_method("display_#{arg}") do
+          display_money(self[arg])
+        end
+      end
+    end
+  end
+end
