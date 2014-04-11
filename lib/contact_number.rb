4f8e45662725fcb8338660a6d4ff9c1faa264d5b
Kronos493 <patchara.ing@gmail.com>
2014-04-11 10:50:44 +0700

    Remove number uniqueness

--- lib/contact_number.rb
+++ lib/contact_number.rb
@@ -2,7 +2,7 @@ class ContactNumber < ActiveRecord::Base
   belongs_to :contactable, polymorphic: true
   CONTACT_TYPE = %w[mobile landline fax1 fax2 fax3 ]
   validates :contact_type, inclusion: { :in => CONTACT_TYPE }
-  validates :number, presence: true, uniqueness: true
+  validates :number, presence: true
   validate :number_should_be_correct
 
   CONTACT_TYPE.each do |contact_type|
